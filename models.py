6be7e7dea4f3e207a36d78d474c5c740689e8867
aswinkrishnasunil <127579559+aswinkrishnasunil@users.noreply.github.com>
2023-07-17 16:17:15 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100,null=True)
+class Message(models.Model):
+    value = models.CharField(max_length=10000,null=True)
+    date = models.DateTimeField(default=datetime.now,blank=True)  
+    user = models.CharField(max_length=100000,null=True)
+    room = models.CharField(max_length=100000,null=True)  
